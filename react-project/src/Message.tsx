308049c35a1ea7230d737ca8941ab4c8001ff832
Kavin2908 <kavinkumarg.22eie@kongu.edu>
2024-10-03 11:32:52 +0530

    first commit

---
+++ react-project/src/Message.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Message() {
+    const name = "Lewis Hamilton";
+  return (
+    <div>
+        <h1>Hello {name}</h1>
+    </div>
+  )
+}
+
+export default Message
